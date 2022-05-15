731788c536e4f3fdad721309ad42d5768f534cff
Lailz <lailaabdulraheem@gmail.com>
2022-05-15 20:01:50 +0300

    remove function call

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def greet(
     ),
 ) -> None:
     """Welcome to our awesome CLI app!
-    
+
     Pass in a name either by using the option, or the prompt."""
     bolded = typer.style(name, bold=True)
     typer.secho(f"Hello, {bolded}!", fg=typer.colors.GREEN)
 
 
 if __name__ == "__main__":
-    typer.run(greet())
+    typer.run(greet)
